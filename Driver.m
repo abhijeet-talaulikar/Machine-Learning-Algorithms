a9c8b251a448a32bc495479a9b59961324e4ac8d
Abhijeet Talaulikar <abhijeetstalaulikar@gmail.com>
2017-05-20 17:54:07 +0530

    Add files via upload

---
+++ Driver.m
@@ -0,0 +1,14 @@
+clc
+
+data = csvread('IRIS.csv');
+data = data(randperm(end),:);
+
+x_train = data(1:90,1:end-1);
+y_train = data(1:90,end);
+
+x_test = data(91:end,1:end-1);
+y_test = data(91:end,end);
+
+clf = KNearestNeighbors(x_train,y_train,1.5);
+y_pred = clf.Predict(x_test)
+sum(y_pred == y_test)*100/size(y_pred,1)
