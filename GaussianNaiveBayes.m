ecd27c1769fe3e26e8359f917f4cd6a486d319e1
Abhijeet Talaulikar <abhijeetstalaulikar@gmail.com>
2017-05-22 11:24:38 +0530

    Add files via upload

---
+++ GaussianNaiveBayes.m
@@ -0,0 +1,38 @@
+classdef GaussianNaiveBayes
+    properties
+        nSamples
+        nFeatures
+        Classes
+        x_Train
+        y_Train
+        Py
+    end
+    methods
+        function clf = GaussianNaiveBayes(x_train,y_train)            
+            [clf.nSamples,clf.nFeatures] = size(x_train);
+            clf.Classes = unique(y_train);
+            clf.x_Train = x_train;
+            clf.y_Train = y_train;
+            for i = 1:numel(clf.Classes)
+                clf.Py(i,1) = sum(clf.y_Train == clf.Classes(i)) / clf.nSamples;
+            end
+        end
+        function y_pred = Predict(clf, x_test)
+            y_pred = zeros(size(x_test,1),1);
+            for i = 1:size(x_test,1)
+                Class_Layer = zeros(size(clf.Classes,1),1);
+                for j = 1:numel(clf.Classes)
+                    Class_Layer(j) = clf.Py(j);
+                    classSet = clf.x_Train(clf.y_Train == clf.Classes(j),:);
+                    for k = 1:clf.nFeatures
+                        mu = mean(classSet(:,k));
+                        sigma = std(classSet(:,k));
+                        Class_Layer(j) = Class_Layer(j) * normpdf(x_test(i,k),mu,sigma);
+                    end
+                end
+                [~,id] = max(Class_Layer);
+                y_pred(i) = clf.Classes(id);
+            end
+        end
+    end
+end
